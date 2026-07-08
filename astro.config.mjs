a88a312b58b29aa154fd9ecb87f95a5d285530a7
Tomasz Kazulak <tomkazulak@gmail.com>
2026-07-08 22:39:35 +0200

    Deploy fix.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  base: '/find-country-for-phd/',
   build: {
     assets: 'assets'
   }
